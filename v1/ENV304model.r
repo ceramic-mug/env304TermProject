1403ece2c5a01ed507b7376e21fa3a9d9279792d
ceramic-mug <ceramic_mug@outlook.com>
2020-10-26 18:01:04 -0400

    setting up the script

---
+++ v1/ENV304model.r
@@ -0,0 +1,15 @@
+# clean the workspace so we don't have confounding variables
+rm(list=ls())
+# Define parameters
+
+
+
+# Define timescale and number of counties
+
+# Build arrays for storing model output
+
+# Build matrix
+
+# Setup model output file
+
+# Build main loop
